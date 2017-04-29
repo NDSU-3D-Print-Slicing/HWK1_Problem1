6cc03c86c91f04e6d2c153c045fc00f1b77b0242
ryanquinnnelson <ryan.q.nelson@gmail.com>
2017-04-28 22:38:15 -0500

    Finished formatting for delivery.

--- Grouper/src/Grouper/CostCalculator.java
+++ Grouper/src/Grouper/CostCalculator.java
@@ -7,13 +7,16 @@ package Grouper;
  */
 
 /**
- *
+ * NDSU-JPL 3D Printing
+ * Problem 1
+ * Algorithm to calculate full cost of a single most-efficient tree.
+ * 4-28-17
  * @author Ryan
  */
 public class CostCalculator {
     
     /**
-     * Calculates the cost of a single tree that has a stem.
+     * Calculates the cost of a single tree.
      * @param W width in grid points
      * @param H height in grid points
      * @return cost of tree structure
@@ -75,6 +78,13 @@ public class CostCalculator {
         }//end if
     }
     
+    /**
+     * Helper function to calculate the cost of the leftover structure in given subtree.
+     * @param W width in grid points of given subtree
+     * @param H height in grid points of full grouping of given subtree
+     * @param subtrees integer array represents two subtrees of this subtree
+     * @return cost of leftover structure
+     */
     public static double costLeftover(int W, int H, int [] subtrees){
         
         double total = 0;
@@ -153,115 +163,10 @@ public class CostCalculator {
         
         return cost;
     }
- 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    /*
-    
-    //old version
-    public static double total(int W, int H){
-        
-        //note: does not yet work for trees of height 3 pts only!!
-        //base case
-        if(W < 5){
-            switch(W){
-                case 1:
-                    return 1;
-                case 2:
-                    return Math.sqrt(2)+1;   
-                case 3:
-                    return 2*Math.sqrt(2)+1;    
-                case 4:
-                    return 3*Math.sqrt(2)+3;   
-                default:
-                    return 0; //should never occur
-            }//end switch
-        }//end if
-        else { //recursive step
-            
-            double sub1 = 0;
-            double sub2 = 0;
-            double sub3 = 0;
-            
-            //calculate height of full grouping
-            int fullGroupHeight = (int) Math.floor(W/2) + 1;
-            
-            //subdivide width into two subtrees
-            int leftSubWidth = (int) Math.floor(W/2);
-            int rightSubWidth = W - leftSubWidth;
-            
-            //check for even + even and change to odd + odd or odd + even
-            if(leftSubWidth % 2 == 0 && rightSubWidth % 2 == 0){
-                leftSubWidth -= 1;
-                rightSubWidth = W - leftSubWidth;
-            }
-            
-            //System.out.println("left: " + leftSubWidth + ", right: " + rightSubWidth);
-            
-            //calculate leftover
-            sub1 = leftover(W,H, leftSubWidth, rightSubWidth);
-            
-            //continue subdividing
-            sub2 = total(leftSubWidth, fullGroupHeight);
-            sub3 = total(rightSubWidth, fullGroupHeight);
-            //System.out.println("sub1: " + sub1 + "\nsub2: " + sub2 + "\nsub3: " + sub3 + "\n\n");
-            return sub1 + sub2 + sub3;
-        }    
-    }//end total
-    
-    
-    
-    
-    
-    
-    public static double leftover(int W, int H, int leftWidth, int rightWidth){
 
-        int leftFullGroup = (int) Math.floor(leftWidth/2) + 1;
-        int leftRemainder = H - leftFullGroup;
-        
-        int rightFullGroup = (int) Math.floor(rightWidth/2) + 1;
-        int rightRemainder = H - rightFullGroup;
-        
-        double total = 0;
-        
-        //cases to consider:
-        //stem later
-        //diagonal vee (even)
-        //diagonal vee (uneven)
-        //diagonal cant (even)
-        //diagonal cant (uneven)
-        
-        //Case 1: W is even (we have a cant on the one side) //?? left only??
-        
-        if(W % 2 == 0){
-            int rightRemainderHeight = rightRemainder; //square
-            int leftRemainderHeight = leftRemainder - 1; //square
-            
-            total = rightRemainderHeight * Math.sqrt(2) +
-                    leftRemainderHeight * Math.sqrt(2) + 1;
-            
-        }
-        else{ //W is odd (no cant on either side)
-            int rightRemainderHeight = rightRemainder;
-            int leftRemainderHeight = leftRemainder;
-            
-            total = rightRemainderHeight * Math.sqrt(2) +
-                    leftRemainderHeight * Math.sqrt(2);
-        }     
-        return total;  
-    }   
-*/
-    
+    //testing
     public static void main(String[] args) {
-        System.out.println(treeCost(15,14));
+        System.out.println(treeCost(15,13));
     }
 }
 
